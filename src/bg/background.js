4a0f14d2cdd56dc4218d8839da3159d7ba12d2ee
vilnis <vilnis@es.lv>
2018-03-31 13:18:12 +0300

    extension init

---
+++ src/bg/background.js
@@ -0,0 +1,13 @@
+// if you checked "fancy-settings" in extensionizr.com, uncomment this lines
+
+// var settings = new Store("settings", {
+//     "sample_setting": "This is how you use Store.js to remember values"
+// });
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // No tabs or host permissions needed!
+    console.log('Turning ' + tab.url + ' red!');
+    chrome.tabs.executeScript({
+        code: 'document.body.style.backgroundColor="red"'
+    });
+});
---
+++ src/inject/inject.js
@@ -0,0 +1,6 @@
+console.log(123);
+
+$('body').on('click', function(){
+	console.log(321);
+	$('body').scrollTop(200);
+});
