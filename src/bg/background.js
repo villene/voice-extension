80a2b0adea2d9fc43bd55c7915f71629c1c795eb
vilnis <vilnis@es.lv>
2018-04-04 00:19:47 +0300

    websocket begin

--- src/bg/background.js
+++ src/bg/background.js
@@ -3,6 +3,25 @@
 // var settings = new Store("settings", {
 //     "sample_setting": "This is how you use Store.js to remember values"
 // });
+//
+// chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+//     $.get("https://www.gstatic.com/firebasejs/4.12.1/firebase.js", function(result) {
+//         chrome.tabs.executeScript(tabs[0].id, {code: result});
+//         config = {
+//             apiKey: "AIzaSyCN2On1hWv0Qqa9TX-KnIYZQ_9LE7YHxqo",
+//             authDomain: "speech2browser.firebaseapp.com",
+//             databaseURL: "https://speech2browser.firebaseio.com",
+//             projectId: "speech2browser",
+//             storageBucket: "speech2browser.appspot.com",
+//             messagingSenderId: "666598580580"
+//         };
+//
+//         firebase.initializeApp(config);
+//
+//         console.log(firebase);
+//         speechdb = firebase.database().ref();
+//     }, "text");
+// });
 
 chrome.browserAction.onClicked.addListener(function(activeTab){
     var newURL = "http://stackoverflow.com/";
--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,11 +1,30 @@
-console.log(chrome);
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyCN2On1hWv0Qqa9TX-KnIYZQ_9LE7YHxqo",
+    authDomain: "speech2browser.firebaseapp.com",
+    databaseURL: "https://speech2browser.firebaseio.com",
+    projectId: "speech2browser",
+    storageBucket: "speech2browser.appspot.com",
+    messagingSenderId: "666598580580"
+};
 
-console.log(123);
+firebase.initializeApp(config);
+
+console.log(firebase);
+var speechdb = firebase.database().ref();
+
+speechdb.on('value', (snapshot) => {
+    console.log(snapshot.val());
+    // chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+    //     console.log(response.farewell);
+    // });
+});
+
 $('body').on('click', function(){
-	console.log($().jquery);
-	// $(window).scrollTop(200);
-
-    chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-        console.log(response.farewell);
-    });
+    // console.log($().jquery);
+    // // $(window).scrollTop(200);
+    //
+    // chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
+    //     console.log(response.farewell);
+    // });
 });
